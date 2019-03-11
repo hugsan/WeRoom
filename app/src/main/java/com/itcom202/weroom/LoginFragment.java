2562fd62a1315eedd6c8dfbc845a30af4a5b48c4
PatrickVibild <44056401+patrickvibild@users.noreply.github.com>
2019-03-11 11:26:29 +0100

    Create LoginFragment and single fragment abstract class

---
+++ app/src/main/java/com/itcom202/weroom/LoginFragment.java
@@ -0,0 +1,13 @@
+package com.itcom202.weroom;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
