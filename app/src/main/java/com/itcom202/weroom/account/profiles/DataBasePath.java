5999dc8a1fe03fd58f984112ae01e83be69e498c
PatrickVibild <44056401+patrickvibild@users.noreply.github.com>
2019-03-19 14:26:31 +0100

    Commets

--- app/src/main/java/com/itcom202/weroom/account/profiles/DataBasePath.java
+++ app/src/main/java/com/itcom202/weroom/account/profiles/DataBasePath.java
@@ -1,6 +1,8 @@
 package com.itcom202.weroom.account.profiles;
 
 public class DataBasePath {
+
+    //THIS IS An ENUM
     public static final String USERS = "users";
     public static final String PROFILE = "profile";
 
