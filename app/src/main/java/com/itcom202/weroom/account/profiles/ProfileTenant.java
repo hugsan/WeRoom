d96aba263e40ec8b4f55ef62eebd067f4ed7ba92
ChristianHolfelt <cholfe18@student.aau.dk>
2019-03-27 15:25:07 +0100

    Made a layout for landlord and tenant

---
+++ app/src/main/java/com/itcom202/weroom/account/profiles/ProfileTenant.java
@@ -0,0 +1,15 @@
+package com.itcom202.weroom.account.profiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.itcom202.weroom.R;
+
+public class ProfileTenant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_tenant);
+    }
+}
