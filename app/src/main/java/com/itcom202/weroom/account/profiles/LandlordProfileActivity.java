7fa65121a4be52f457dda81bcff35db46af5f0a6
ChristianHolfelt <cholfe18@student.aau.dk>
2019-03-30 11:48:10 +0100

    Changed layout for tenant and landlord profile and made backups

---
+++ app/src/main/java/com/itcom202/weroom/account/profiles/LandlordProfileActivity.java
@@ -0,0 +1,18 @@
+package com.itcom202.weroom.account.profiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import com.itcom202.weroom.SingleFragmentActivity;
+
+public class LandlordProfileActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new LandlordProfileFragment();
+    }
+    public static Intent newIntent(Context myContext){
+        Intent i = new Intent(myContext, LandlordProfileActivity.class);
+        return i;
+    }
+}
