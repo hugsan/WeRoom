e13f2887ed8b75e90e5b3bb1f2f6d63e8b35abd5
LoreVonElbeland <44893161+LoreVonElbeland@users.noreply.github.com>
2019-04-10 18:43:18 +0200

    tenant_profile_fragment.xml will replace profile_tenant_fragment.xml

--- app/src/main/java/com/itcom202/weroom/MainActivity.java
+++ app/src/main/java/com/itcom202/weroom/MainActivity.java
@@ -28,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
         //if there is no user logged in Firebase it starts LoginActivity
         if (firebaseAuth.getCurrentUser() == null){
             Log.i(TAG,"We are not logged as:");
-            startActivity(SwipeActivity.newIntent(this));
+            startActivity(LoginActivity.newIntent(this));
             finish();
         }//IF there is a user logged into Firebase it starts at AccountCreationActivity
         else{
             Log.i(TAG,"We are logged"+ firebaseAuth.getCurrentUser().getEmail());
-            startActivity(LoginActivity.newIntent(this));
+            startActivity(Profile_Activity.newIntent(this));
             finish();
         }
 
